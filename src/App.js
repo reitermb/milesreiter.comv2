899d26f10ade605372a5f6b964fa72f50458e450
Miles Reiter <reitermb@gmail.com>
2024-10-10 22:42:29 -0400

    Update App.js

--- src/App.js
+++ src/App.js
@@ -3,10 +3,22 @@ import './App.css';
 
 function App() {
   return (
-<div>
-    <h1>Miles Reiter</h1>
-    <p>Testing</p>
-    </div
+ <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
   );
 }
 
